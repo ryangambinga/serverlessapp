a575f6acad16676bcc54443918b7ea5e5df380d1
ryangambinga <59413049+ryangambinga@users.noreply.github.com>
2022-11-13 23:14:10 +0200

    Initial commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'xj1fuuwb65';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-fxncpuejmhukdjl6.us.auth0.com',
+  clientId: 'X2sIbN8E3rBk32oEbE0MkVpOMp6ovINE',
+  callbackUrl: 'http://localhost:3000/callback'
+};
